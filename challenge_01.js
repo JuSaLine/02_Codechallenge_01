6b4d09fd5ff24fb62fee145f1b717522fb623876
Julia <julchik.sa@gmail.com>
2021-05-18 15:29:53 +0200

    done

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,9 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
+
+ersteZahl = prompt("Geben Sie bitte die erste Zahl ein: ");
+zweiteZahl = prompt("Geben Sie bitte die zweite Zahl ein: ");
+
+summe = parseInt(ersteZahl) + parseInt(zweiteZahl);
+console.log("Die Summe der Zahlen ist: " + summe);
